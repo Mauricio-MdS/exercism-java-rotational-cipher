b184c27938fa75cd1d217409b3760de39afba30c
Mauricio MdS <45921381+Mauricio-MdS@users.noreply.github.com>
2024-05-27 11:38:58 -0300

    Exercise solution

--- src/main/java/RotationalCipher.java
+++ src/main/java/RotationalCipher.java
@@ -1,11 +1,26 @@
 class RotationalCipher {
 
+    private final int shiftKey;
+
     RotationalCipher(int shiftKey) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        this.shiftKey = shiftKey % 26;
     }
 
     String rotate(String data) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        return data.chars()
+                .map(this::rotateChar)
+                .collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append)
+                .toString();
+    }
+
+    private int rotateChar(int c) {
+        return Character.isLetter(c) ? rotateLetter(c) : c;
+    }
+
+    private int rotateLetter(int c) {
+        char initialLetter = Character.isUpperCase(c) ? 'A' : 'a';
+        return (c - initialLetter + shiftKey) % 26 + initialLetter;
+
     }
 
 }
